18e7b7448566f76e3042a32786861576c0aacbfc
AncientMeme <hatredwraith@gmail.com>
2024-04-29 18:42:38 +1000

    Fully implemented Encoding and Decoding functionalities

--- CompressionTool/src/CompressionTool/Program.cs
+++ CompressionTool/src/CompressionTool/Program.cs
@@ -3,11 +3,11 @@
 // Setup command line tool
 var fileArgument = new Argument<FileInfo?>(
   name: "file",
-  description: "The file to be compressed"
+  description: "The target file"
 );
 var outputArgument = new Argument<string?>(
   name: "output",
-  description: "The name of the compressed file"
+  description: "The output file"
 );
 
 // Sub commands
@@ -15,10 +15,17 @@ var encodeCommand = new Command("encode", "Compress target file using Huffman en
 encodeCommand.AddArgument(fileArgument);
 encodeCommand.AddArgument(outputArgument);
 encodeCommand.SetHandler(CompressFile, fileArgument, outputArgument);
+var decodeCommand = new Command("decode", "Decode compressed file using Huffman encoding");
+decodeCommand.AddArgument(fileArgument);
+decodeCommand.AddArgument(outputArgument);
+decodeCommand.SetHandler(DecompressFile, fileArgument, outputArgument);
 
 // Root command
-var rootCommand = new RootCommand("Command line tool to compress and decompress files");
-rootCommand.Add(encodeCommand);
+var rootCommand = new RootCommand("Command line tool to compress and decompress files")
+{
+  encodeCommand,
+  decodeCommand,
+};
 
 await rootCommand.InvokeAsync(args);
 
@@ -41,15 +48,42 @@ void CompressFile(FileInfo? info, string? outputName)
     return;
   }
 
-  // Read File
-  string fileContent; 
-  using (FileStream stream = info.OpenRead())
-  using (StreamReader sr = new(stream))
+  // Read file content
+  string content;
+  using(FileStream file = info.OpenRead())
+  using(StreamReader sr = new(file))
   {
-    fileContent = sr.ReadToEnd();
+    content = sr.ReadToEnd();
   }
 
   // Compress File
   var manager = new CompressionManager();
-  manager.CompressFile(fileContent, outputName);
+  manager.Compress(content, outputName);
+}
+
+void DecompressFile(FileInfo? info, string? outputName)
+{
+  if (info == null)
+  {
+    Console.WriteLine("File argument cannot be null");
+    return;
+  }
+  if (!info.Exists)
+  {
+    Console.WriteLine("The file was not found");
+    return;
+  }
+  if (outputName == null)
+  {
+    Console.WriteLine("Output file required");
+    return;
+  }
+
+  // Get file stream
+  using(FileStream stream = info.OpenRead())
+  {
+     // Decompress File
+    var manager = new CompressionManager();
+    manager.Decompress(stream, outputName);
+  }
 }
--- CompressionTool/src/CompressionTool/Util/CompressionManager.cs
+++ CompressionTool/src/CompressionTool/Util/CompressionManager.cs
@@ -3,69 +3,164 @@ using System.Text;
 
 public class CompressionManager
 {
-  public void CompressFile(string content, string outputFileName)
+  public CompressionManager()
   {
-    // Generate Header
+
+  }
+
+  public void Compress(string content, string outputFileName)
+  {
+    // Get encode table
     var charFrequency = CharCounter.GetCharFrequency(content);
-    string header = GenerateEncodingHeader(charFrequency);
+    var tree = HuffmanTreeBuilder.BuildTree(charFrequency);
+    var encodeTable = HuffmanEncoder.GetEncodingTable(tree);
 
-    // Generate Encoded Content
-    var treeBuilder = new HuffmanTreeBuilder();
-    var HuffmanTree = treeBuilder.BuildTree(charFrequency);
-    var encoder = new HuffmanEncoder();
-    var encodeTable = encoder.GetEncodingTable(HuffmanTree);
-    byte[] body = GenerateEncodedContent(content, encodeTable);
-
-    using(FileStream outputFile = File.Open(outputFileName, FileMode.Create, FileAccess.Write))
-    using(StreamWriter writer = new(outputFile))
-    {
-      writer.Write(header);
-      writer.Flush();
-      outputFile.Write(body);
-    }
-  }
-
-  private string GenerateEncodingHeader(Dictionary<char, int> charFrequency)
-  {
-    StringBuilder header = new();
-    foreach(KeyValuePair<char, int> entry in charFrequency)
-    {
-      string section = $"{entry.Key}:{entry.Value},";
-      header.Append(section);
-    }
-    // replace final comma with line break
-    header.Remove(header.Length - 1, 1);
-    header.Append('\n');
-
-    return header.ToString();
-  }
-
-  private byte[] GenerateEncodedContent(string content, Dictionary<char, string> encodeTable)
-  {
-    StringBuilder body = new();
-    // Convert char to binary strings
-    foreach (char c in content)
-    {
-      body.Append(encodeTable[c]);
-    }
-    // Ensure the content length is multiple of 8 (one byte)
-    while(body.Length % 8 != 0)
-    {
-      body.Append('0');
-    }
-    string binaryString = body.ToString();
+    // Pack content into bytes
+    var header = GetHeaderInBytes(charFrequency);
+    var body = HuffmanEncoder.Encode(content, encodeTable);
     
-    // Convert binary strings to chunks of bytes
-    var bytes = new List<byte>();
-    int index = 0;
-    while (index < binaryString.Length)
+    // Write bytes to output file
+    List<byte> output = new();
+    output.AddRange(header);
+    output.AddRange(body);
+    using(FileStream fs = File.Open(outputFileName, FileMode.Create, FileAccess.Write))
     {
-      string chunk = binaryString.Substring(index, 8);
-      int chunkValue = Convert.ToInt32(chunk, 2);
-      bytes.Add((byte)chunkValue);
-      index += 8;
+      fs.Write(output.ToArray());
+    }
+  }
+
+  public void Decompress(FileStream stream, string outputFileName)
+  {
+    // Seperate header and body
+    var charFrequency = GetCharFrequency(stream);
+    string body = GetBody(stream);
+
+    // Get Decoding Table
+    var tree = HuffmanTreeBuilder.BuildTree(charFrequency);
+    var encodingTable = HuffmanEncoder.GetEncodingTable(tree);
+    var decodingTable = HuffmanEncoder.GetDecodingTable(tree);
+
+    // Get True Length for body then decode
+    int bodyLength = GetBodyLength(charFrequency, encodingTable);
+    string content = DecodeBody(body, bodyLength, decodingTable);
+
+    // Write content in output file
+    using(FileStream file = File.Open(outputFileName, FileMode.Create, FileAccess.Write))
+    using(StreamWriter writer = new(file))
+    {
+      writer.Write(content);
+    }
+  }
+
+  private byte[] GetHeaderInBytes(Dictionary<char, int> frequency)
+  {
+    // Translate header
+    StringBuilder sb = new();
+    foreach (KeyValuePair<char, int> entry in frequency)
+    {
+      sb.Append($"{Convert.ToInt16(entry.Key)}:{entry.Value},");
+    }
+    sb.Append('|'); // Signals header end
+
+    // Turn into bytes
+    var headerBytes = sb.ToString().Select(c => (byte)c);
+    List<byte> bytes = new();
+    bytes.AddRange(headerBytes);
+    return bytes.ToArray();
+  }
+
+  private Dictionary<char, int> GetCharFrequency(FileStream stream)
+  {
+    StringBuilder sb = new();
+    bool gotSeperator = false;
+    while (!gotSeperator)
+    {
+      int b = stream.ReadByte();
+      // Reach end of file without seperator
+      if (b == -1)
+      {
+        Console.WriteLine("This is not a Compressed File using Huffman Encoding");
+        Environment.Exit(1);
+      }
+
+      char c = (char)b;
+      if (c == '|')
+      {
+        gotSeperator = true;
+      }
+      else
+      {
+        sb.Append(c);
+      }
     }
 
-    return bytes.ToArray();
+    // Convert Header into Frequency Table
+    string[] rawData = sb.ToString().Split(',', StringSplitOptions.RemoveEmptyEntries);
+    var charFrequency = new Dictionary<char, int>();
+    foreach(string entry in rawData)
+    {
+      string[] data = entry.Split(':');
+      char key = Convert.ToChar(Convert.ToInt16(data[0], 10));
+      int value = Convert.ToInt32(data[1]);
+      charFrequency.Add(key, value);
+    }
+    return charFrequency;
+  }
+
+  private string GetBody(FileStream stream)
+  {
+    StringBuilder sb = new();
+    bool streamEnd = false;
+    while(!streamEnd)
+    {
+      int b = stream.ReadByte();
+      if (b == -1)
+      {
+        streamEnd = true;
+      }
+      else
+      {
+        string byteString = Convert.ToString(b, 2).PadLeft(8, '0');
+        sb.Append(byteString);
+      }
+    }
+
+    return sb.ToString();
+  }
+
+  private int GetBodyLength(Dictionary<char, int> charFrequency, Dictionary<char, string> table)
+  {
+    int length = 0;
+    foreach (KeyValuePair<char, int> entry in charFrequency)
+    {
+      int frequency = entry.Value;
+      int codeLength = table[entry.Key].Length;
+      length += frequency * codeLength;
+    }
+    return length;
+  }
+
+  private string DecodeBody(string body, int bits, Dictionary<string, char> table)
+  {
+    StringBuilder sb = new();
+    string bitString = body[..bits];
+    int index = 0;
+    int size = 1;
+    while(index + size <= bitString.Length)
+    {
+      string code = bitString.Substring(index, size);
+      if (table.ContainsKey(code))
+      {
+        sb.Append(table[code]);
+        index += size;
+        size = 1;
+      }
+      else
+      {
+        size++;
+      }
+    }
+
+    return sb.ToString();
   }
 }
