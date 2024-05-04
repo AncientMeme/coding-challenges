cc3330baea28aeb6374db3af8f4b103a4c195f5a
AncientMeme <hatredwraith@gmail.com>
2024-05-04 21:42:58 +1000

    Added -f option parsing

--- Cut/src/Cut/Program.cs
+++ Cut/src/Cut/Program.cs
@@ -1,3 +1,7 @@
 ﻿using System.CommandLine;
 
+var fieldOption = new Option<int>(
+  name: "--field",
+  description: "The field to select, also prints lines without delimiter characters"
+);
 var rootCommand = new RootCommand("A tool for cutting selected portion from each line in a file");
