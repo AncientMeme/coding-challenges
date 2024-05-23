ef70d8e677dc481877d8880bfd31a75e1e2059c3
AncientMeme <hatredwraith@gmail.com>
2024-05-23 22:11:20 +0800

    Setup Calculator project

---
+++ Calculator/src/Calculator/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System.CommandLine;
+
+var inputArgument = new Argument<string>(
+  name: "expression",
+  description: "The expression to calculate",
+  getDefaultValue: () => {return "";}
+);
+
+var rootCommand = new RootCommand("Calculates the math expression and outputs the result");
+rootCommand.AddArgument(inputArgument);
+rootCommand.SetHandler(CalculatorHandler, inputArgument);
+
+await rootCommand.InvokeAsync(args);
+
+void CalculatorHandler(string input)
+{
+  if (input == "")
+  {
+    Console.WriteLine("Please input a math expression");
+    return;
+  }
+  Console.WriteLine($"{input}");
+}
