e3c27937a979d39aeafa2420e006da0f64ce7bf2
AncientMeme <hatredwraith@gmail.com>
2024-05-24 18:24:16 +0800

    Adding docstring to Lexer

--- Calculator/src/Calculator/Util/Lexer.cs
+++ Calculator/src/Calculator/Util/Lexer.cs
@@ -1,11 +1,9 @@
 
-// Takes apart user input and construct tokens
-
-using System.Diagnostics.CodeAnalysis;
-using System.Text;
-
 namespace Util;
 
+/// <summary>
+/// Enum <c>TokenType</c> Value to identify the type of Token
+/// </summary>
 public enum TokenType
 {
   Number,
@@ -13,9 +11,19 @@ public enum TokenType
   LeftBracket,
   RightBracket,
 }
+/// <summary>
+/// Token that represents a number or operation.
+/// </summary>
 public struct Token : IEquatable<Token>
 {
+  /// <value>
+  /// The value of the token, could be an number or operation
+  /// </value>
   public string value;
+  /// <value>
+  /// The type of token this is
+  /// <see cref="TokenType"/>
+  /// </value>
   public TokenType type;
 
   public Token(string value, TokenType type)
@@ -36,17 +44,33 @@ public struct Token : IEquatable<Token>
     return (type, value).GetHashCode();
   }
 }
+///<summary>
+/// Class <c>Lexer</c> Takes apart user input and construct tokens
+///</summary>
 public class Lexer
 {
+  /// <value>
+  /// The table to lookup precedent for the operation
+  /// </value>
   private readonly Dictionary<string, int> valueTable = new()
   {
     {"+", 2},
     {"-", 2},
     {"*", 3},
     {"/", 3},
+    {"^", 4},
   };
+  /// <value>
+  /// The token queue to enqueue all generated token
+  /// </value>
   private Queue<Token> tokens;
+  /// <value>
+  /// The math expression in string form from the user
+  /// </value>
   private string input;
+  /// <value>
+  /// The index pointing at the character in <c>input</c>
+  /// </value>
   private int index;
   public Lexer(string input)
   {
@@ -55,11 +79,18 @@ public class Lexer
     this.index = 0;
   }
 
+  /// <summary>
+  /// Returns a new <c>Queue</c> for parser to read. <see cref="Parser"/>
+  /// </summary>
+  /// <returns>A <c>Queue</c> containing <c>Token</c> in Reverse Polish Notation.</returns>
+  /// <exception cref="InvalidDataException">
+  /// When the input contains a character that is not a number or operation
+  /// </exception>
   public Queue<Token> GetTokens()
   {
     tokens = new();
     index = 0;
-    
+    tokens = new();
     Stack<Token> operators = new(); // monotonic increase stack
     char c;
     while(index < input.Length)
@@ -90,6 +121,10 @@ public class Lexer
     return tokens;
   }
 
+  /// <summary>
+  /// Read the section of number in <c>input</c> and return it
+  /// </summary>
+  /// <returns>The number inside <c>input</c></returns>
   private string readNumber()
   {
     string validCharacters = "0123456789.";
@@ -103,6 +138,11 @@ public class Lexer
     return input.Substring(startIndex, length);
   }
 
+  /// <summary>
+  /// Process a token by pushing into the stack and enqueue the previous tokens
+  /// </summary>
+  /// <param name="operators">The reference to the operator stack</param>
+  /// <param name="token">The token to be inserted</param>
   private void ProcessOperators(Stack<Token> operators, Token token)
   {
     int precedent = valueTable[token.value];
